cb3bc57372014e42210d05804f104e9230cf078d
Aerodos12 <weldon.henson97@gmail.com>
2021-08-17 15:04:37 -0400

    New build syste

---
+++ scripts/Windows/SetupEngineDeps.bat
@@ -0,0 +1,19 @@
+@echo off
+
+echo "Setting up dependencies..."
+pushd ..\..\
+    echo "1) GLFW"
+    git clone https://github.com/TheCherno/GLFW RodskaEngine/vendor/GLFW
+    echo "2) imgui"
+    git clone https://github.com/CrossforceStudios/imgui RodskaEngine/vendor/imgui
+    echo "3) glm"
+    git clone https://github.com/CrossforceStudios/glm RodskaEngine/vendor/glm
+    echo "4) tinyobjloader"
+    git clone https://github.com/CrossforceStudios/tinyobjloader RodskaEngine/vendor/tinyobjloader
+    echo "5) yaml-cpp"
+    git clone https://github.com/CrossforceStudios/yaml-cpp RodskaEngine/vendor/yaml-cpp
+    echo "6) spdlog"
+    git clone git://github.com/gabime/spdlog.git RodskaEngine/vendor/spdlog
+    echo "Done."
+popd
+pause
